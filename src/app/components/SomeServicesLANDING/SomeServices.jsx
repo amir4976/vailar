c7369cbce3de5dfa4d252dd2c7a5bd84e4dfee14
amir4976 <amirhosianahmadi4976@gmail.com>
2023-08-24 18:41:27 +0330

    some servises component

--- src/app/components/SomeServicesLANDING/SomeServices.jsx
+++ src/app/components/SomeServicesLANDING/SomeServices.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import styles from './SomeServices.module.css'
+import ServicesCard from '../servicesCard/ServicesCard'
+
+
+
 
 function SomeServices() {
   return (
-    <div>
-      
+    <div className={styles.SomeServicesWarper}>
+        <div className={styles.someServicesTitle}>
+            <h2>برخی از خدمات شرکت</h2>
+        </div>
+        <div className={styles.someServicesDesc}>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+        </div>
+        <div className="row">
+        <ServicesCard/>
+        <ServicesCard/>
+        <ServicesCard/>
+        <ServicesCard/>
+        </div>
     </div>
+
   )
 }
 
---
+++ src/app/components/servicesCard/ServicesCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServicesCard() {
+  return (
+    <div className='col-md-3'>
+           . Architecto?
+    </div>
+  )
+}
+
+export default ServicesCard
